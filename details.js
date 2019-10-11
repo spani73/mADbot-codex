4d3fe2f667bde017f949a77e1b10969b384144dc
Subhankar Panigrahi <52607552+subhu73@users.noreply.github.com>
2019-10-11 12:49:57 +0530

    Delete details.js

--- details.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    consumer_key:   'RvQ0wK0QoaNkJO52XYVBCD1AS', 
-    consumer_secret:    'iX4W2kzT1tzhFKzcsbYg1U2GeGZX9uV5t9yQ7MoSRfiSkSpAb4',
-    access_token:         '1175124894012473344-0G3c6EGEJ6WTHZSWT9ISdIpuPkQQGc',
-    access_token_secret:  'KBALA0O2mfqpkWSDRb1ptqa5ixcVOUIZzVXKgHUiaz3dG',
-    username:'9348920602',
-    password:'sipun@123'
-}
